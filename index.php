63750339afc02a388d08d433e5dc08874fd6d7ef
Dounayha <dounayha05@gmail.com>
2024-08-23 10:45:25 +0200

    slider

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
- 
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="./assets/images/CSS/index.css" rel="stylesheet">
+ </head>
+ <body>
+
+    <div class="slider">
+        <img src="./assets/images/slider.jpg" class="slider-background">
+    </div>
+        <section class="slider-content">
+            <h1>Dounayha Abdillah</h1>
+            <p> Je suis developpeur full-stack</p>
+        </section>
+       
+    
+ </body>
+ </html>
